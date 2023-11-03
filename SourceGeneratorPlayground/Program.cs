314bda3e4c2ac3401e530c156fd616500e53e8a8
ewerspej <ewerspej@tcd.ie>
2023-11-03 10:33:32 +0100

    first generator

---
+++ SourceGeneratorPlayground/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SourceGeneratorPlayground;
+
+static partial class Program
+{
+    static void Main(string[] args)
+    {
+        Hello("Generator");
+    }
+
+    static partial void Hello(string message);
+}

9ab64b942aaac4ba5d8a718f02f90c75ab72d62c
ewerspej <ewerspej@tcd.ie>
2023-11-20 12:02:54 +0100

    removed some types

--- SourceGeneratorPlayground/Program.cs
+++ SourceGeneratorPlayground/Program.cs
@@ -5,7 +5,7 @@ static partial class Program
     static void Main(string[] args)
     {
         Hello("Generator");
-        Hello(BedTypes.PlumBed);
+        Hello(BedTypes.BunkBed);
 
         foreach (var bedType in BedTypes.AllBedTypes)
         {
